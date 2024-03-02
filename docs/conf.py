2e8a43be3b3f94128ca4664881b7e0a53e35970e
David Runemalm <david.runemalm@gmail.com>
2024-03-02 14:07:38 +0700

    Bump version to 1.0.0-alpha.3.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ copyright = '2024, David Runemalm'
 author = 'David Runemalm'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-alpha.2'
+release = '1.0.0-alpha.3'
 
 
 # -- General configuration ---------------------------------------------------
