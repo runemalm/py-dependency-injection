db4c185b4a4cfa74ad1fa95b2b024ab590d39a10
David Runemalm <david.runemalm@gmail.com>
2025-06-22 14:27:12 +0200

    Add custom sidebar template.

--- docs/conf.py
+++ docs/conf.py
@@ -93,6 +93,15 @@ autodoc_member_order = "alphabetical"
 #
 html_theme = "sphinx_rtd_theme"
 
+html_context = {
+    "display_github": False,  # Optional: only needed if you're showing GitHub links
+    "display_version": True,  # ✅ Enable version display manually
+    "version": version,  # pulled from your __version__
+    "release": release,
+}
+
+html_css_files = ["custom.css"]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
